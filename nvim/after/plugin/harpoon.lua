d7efd07f1b7f475da005ca846736825fe5cdb840
andysolomon <andrewsolomon.edu@gmail.com>
2024-01-28 03:23:13 -0500

    add harpoon keymaps

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,10 +1,15 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "ha", mark.add_file)
+vim.keymap.set("n", "hd", mark.rm_file)
+vim.keymap.set("n", "hc", mark.clear_all)
+vim.keymap.set("n", "hn", ui.nav_next)
+vim.keymap.set("n", "hp", ui.nav_prev)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+
