2b0d847c1f933401307acdbc6403d4171d5a23b3
andysolomon <andrewsolomon.edu@gmail.com>
2024-05-06 13:59:04 -0400

    Fixing Tab Stop

--- nvim/lua/andysolomon/set.lua
+++ nvim/lua/andysolomon/set.lua
@@ -5,7 +5,7 @@ vim.opt.guicursor = ""
 vim.opt.binary = true
 vim.opt.eol = false
 
-vim.opt.tabstop = 2
+vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
@@ -13,9 +13,6 @@ vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.cindent = true
-vim.cmd [[
-  filetype plugin indent on
-]]
 
 vim.opt.swapfile = false
 
